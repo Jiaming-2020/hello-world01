847654468a200c037b8247bc81475151b5ba6727
Jiaming-2020 <849519112@qq.com>
2021-01-12 20:12:14 +0800

    修改图片

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
       <h2>预祝0713java全体学生前程似锦</h2>
       <h2>预祝0713java全体学生统统月薪过万</h2>
       <h2>预祝0713java全体学生统统月薪过2万</h2>
-   <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201505%2F21%2F20150521094037_nwWCy.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613039917&t=34c0f7d5507f2d526d2ae6d0f3d366b6"/>
+   <img src="https://jiaming-guli-file.oss-cn-shanghai.aliyuncs.com/hzw.jfif"/>
   </div>
 </template>
 
