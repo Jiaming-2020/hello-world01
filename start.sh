0f7a6e3767df7c6cd7fa20763528d968369cbaa0
Jiaming-2020 <73333799+Jiaming-2020@users.noreply.github.com>
2021-01-12 19:39:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,9 +8,4 @@ npm install --registry=https://registry.npm.taobao.org/ && npm run build
 docker rm -f demo1 &> /dev/null
 
 #启动容器
-docker run -d --restart=on-failure:5 \
-    -p 12365:80 \
-    -v $PWD/dist:/usr/share/nginx/html \
-    --name demo1 nginx
-
-
+docker run -d --restart=on-failure:5 -p 12365:80 -v $PWD/dist:/usr/share/nginx/html --name demo1 nginx
