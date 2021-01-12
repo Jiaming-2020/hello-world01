90ae461c8afc36b5beb77c74583acd457d608db0
Jiaming-2020 <849519112@qq.com>
2021-01-12 19:36:02 +0800

    测试自动化部署2

--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ npm install --registry=https://registry.npm.taobao.org/ && npm run build
 docker rm -f demo1 &> /dev/null
 
 #启动容器
-docker run -d --restart=on-failure:5\
-    -p 8080:80 \
+docker run -d --restart=on-failure:5 \
+    -p 12365:80 \
     -v $PWD/dist:/usr/share/nginx/html \
     --name demo1 nginx
 
