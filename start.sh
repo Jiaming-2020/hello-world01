1513f2b8f7c86489d23ee7eaea4c853611e927b4
Jiaming-2020 <849519112@qq.com>
2021-01-12 19:57:33 +0800

    修改脚本

--- start.sh
+++ start.sh
@@ -8,6 +8,6 @@ echo "删除容器"
 docker rm -f demo1 &> /dev/null
 
 echo "启动容器"
-docker run -d --restart=on-failure:5 12365:80 -v $PWD/dist:/usr/share/nginx/html --name demo1 nginx
+docker run -d --restart=on-failure:5 -p 12365:80 -v $PWD/dist:/usr/share/nginx/html --name demo1 nginx
 
 
