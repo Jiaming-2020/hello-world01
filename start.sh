f063719abf862d068fd1d8ffe06a7d91d8ea16c8
Jiaming-2020 <849519112@qq.com>
2021-01-12 19:52:09 +0800

    打印日志

--- start.sh
+++ start.sh
@@ -1,16 +1,13 @@
-# 拉取代码
+echo "拉取代码"
 git pull
 
-# 安装依赖和打包
+echo "安装依赖和打包"
 npm install --registry=https://registry.npm.taobao.org/ && npm run build
 
-# 删除容器
+echo "删除容器"
 docker rm -f demo1 &> /dev/null
 
-#启动容器
-docker run -d --restart=on-failure:5 \
-    -p 12365:80 \
-    -v $PWD/dist:/usr/share/nginx/html \
-    --name demo1 nginx
+echo "启动容器"
+docker run -d --restart=on-failure:5 12365:80 -v $PWD/dist:/usr/share/nginx/html --name demo1 nginx
 
 
